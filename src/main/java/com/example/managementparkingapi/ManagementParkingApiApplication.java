e73d72af3eae58c59110de0c7ca4bab61504e23a
Volerina-Kurkdjian <valeriabalan1997@gmail.com>
2023-01-21 13:30:51 +0200

    initial commit

---
+++ src/main/java/com/example/managementparkingapi/ManagementParkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.managementparkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementParkingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managementparkingapi/ManagementParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managementparkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
